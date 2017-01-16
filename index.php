8474072998f50273a8448edc92edaf27519499c2
ZMKiller <teran866@gmail.com>
2017-01-16 14:02:49 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,359 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
+    <link rel="stylesheet" href="assets/css/gallery.css">
+    <title>Visitka</title>
+<!---->
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+        <meta name="keywords" content="html5, responsive, image gallery, masonry, picture, images, sizes, fluid, history api, visibility api"/>      
+        <link rel="shortcut icon" href="../favicon.ico"> 
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
+		<script src="assets/js/modernizr.custom.70736.js"></script>
+		<noscript><link rel="stylesheet" type="text/css" href="assets/css/noJS.css"/></noscript>
+		
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-7243260-2']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+		</script>
+		<style>#codrops-ad-wrapper{z-index:9999 !important;}</style>
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+    <script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/modernizr.custom.js"></script>
+	
+
+	
+    <link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css'>
+
+    
+      
+	
+	
+		
+	
+  </head>
+
+  <body data-spy="scroll" data-offset="0" data-target="#theMenu">
+		
+	<!-- Menu -->
+	<nav class="menu" id="theMenu">
+		<div class="menu-wrap">
+			<h1 class="logo">Minimal</a></h1>
+			<i class="icon-remove menu-close"></i>
+			<a href="#home" class="smoothScroll">Home</a>
+			<a href="#about" class="smoothScroll">About</a>
+			<a href="#portfolio" class="smoothScroll">Portfolio</a>
+			<a href="#contact" class="smoothScroll">Contact</a>
+			
+			
+		</div>
+		
+		<!-- Menu button -->
+		<div id="menuToggle"><i class="icon-reorder"></i></div>
+	</nav>
+
+
+	
+	<!-- ========== HEADER SECTION ========== -->
+	<section id="home" name="home"></section>
+	<div id="headerwrap">
+		<div class="container">
+			<div class="logo">
+				<img src="assets/img/logo.png">
+			</div>
+			<br>
+			<div class="row">
+				<h1>Визитка програмиста</h1>
+				<br>
+				<h3>Здравствуйте я Александр</h3>
+				<br>
+				<br>
+				<div class="col-lg-6 col-lg-offset-3">
+				</div>
+			</div>
+		</div><!-- /container -->
+	</div><!-- /headerwrap -->
+	
+	
+	
+	<!-- ========== ABOUT SECTION ========== -->
+	<section id="about" name="about"></section>
+	<div id="f">
+		<div class="container">
+			<div class="row">
+				<h3>Обо мне</h3>
+				<p class="centered"><i class="icon icon-circle"></i><i class="icon icon-circle"></i><i class="icon icon-circle"></i></p>
+				
+				<!-- INTRO INFORMATIO-->
+				<div class="col-lg-6 col-lg-offset-3">
+					<p>Я програмист С#</p>
+					<p>Мой богатый опыт позволил создать 10 проэктов<p>
+					<p>мои супер проэкты ниже</p>	
+					
+				</div>								
+			</div>
+		</div><!-- /container -->
+	</div><!-- /f -->
+	
+
+	<!-- ========== CAROUSEL SECTION ========== -->	
+	<section id="portfolio" name="portfolio"></section>
+	
+	<div id="f">
+		<div class="container">
+			<div class="row centered">
+				<h3>Проекты</h3>
+				
+				<p class="centered"><i class="icon icon-circle"></i><i class="icon icon-circle"></i><i class="icon icon-circle"></i></p>
+				
+				<div class="col-lg-6 col-lg-offset-3">
+					
+				<!--	-->
+   <div class="container">
+		
+			<!-- Codrops top bar -->
+            <div class="codrops-top clearfix">
+              
+                <span class="right">
+                	
+                    </a>
+                </span>
+            </div><!--/ Codrops top bar -->
+			
+			<div class="main">
+				<header class="clearfix">
+				
+					
+
+					<div class="support-note">
+						<span class="note-ie">Sorry, only modern browsers.</span>
+					</div>
+					
+				</header>
+				
+				<div class="gamma-container gamma-loading" id="gamma-container">
+
+					<ul class="gamma-gallery">
+
+						<li>
+							<div data-alt="img01" data-description="<h3>1</h3>" data-max-width="1800" data-max-height="2400">								
+								<div data-src="images/xxlarge/i1.jpg" data-min-width="1000"></div>								
+								<div data-src="images/medium/i1.jpg" data-min-width="200"></div>								
+								<noscript>
+									<img src="images/medium/i1.jpg" alt="img01"/>
+								</noscript>
+							</div>
+						</li>
+						<li>
+							<div data-alt="img02" data-description="<h3>2</h3>" data-max-width="1800" data-max-height="1350">
+								<div data-src="images/xxlarge/i2.jpg" data-min-width="1000"></div>								
+								<div data-src="images/medium/i2.jpg" data-min-width="200"></div>								
+								<noscript>
+									<img src="images/medium/i2.jpg" alt="img01"/>
+								</noscript>
+							</div>
+						</li>
+						<li>
+							<div data-alt="img03" data-description="<h3>3</h3>" data-max-width="1800" data-max-height="1350">
+								<div data-src="images/xxlarge/i3.jpg" data-min-width="1000"></div>								
+								<div data-src="images/medium/i3.jpg" data-min-width="200"></div>								
+								<noscript>
+									<img src="images/medium/i3.jpg" alt="img01"/>
+								</noscript>
+							</div>
+						</li>
+						<li>
+							<div data-alt="img04" data-description="<h3>4</h3>" data-max-width="1800" data-max-height="1350">
+								<div data-src="images/xxlarge/i4.jpg" data-min-width="1000"></div>								
+								<div data-src="images/medium/i4.jpg" data-min-width="200"></div>								
+								<noscript>
+									<img src="images/medium/i4.jpg" alt="img01"/>
+								</noscript>
+							</div>
+						</li>
+						<li>
+							<div data-alt="img05" data-description="<h3>5</h3>" data-max-width="1800" data-max-height="2400">
+								<div data-src="images/xxlarge/i5.jpg" data-min-width="1000"></div>								
+								<div data-src="images/medium/i5.jpg" data-min-width="200"></div>								
+								<noscript>
+									<img src="images/medium/i5.jpg" alt="img01"/>
+								</noscript>
+							</div>
+						</li>
+						<li>
+							<div data-alt="img06" data-description="<h3>6</h3>" data-max-width="1800" data-max-height="1350">
+								<div data-src="images/xxlarge/i6.jpg" data-min-width="1000"></div>								
+								<div data-src="images/medium/i6.jpg" data-min-width="200"></div>								
+								<noscript>
+									<img src="images/medium/i6.jpg" alt="img01"/>
+								</noscript>
+							</div>
+						</li>
+						<li>
+							<div data-alt="img07" data-description="<h3>7</h3>" data-max-width="1800" data-max-height="1350">
+								<div data-src="images/xxlarge/i7.jpg" data-min-width="1000"></div>								
+								<div data-src="images/medium/i7.jpg" data-min-width="200"></div>								
+								<noscript>
+									<img src="images/medium/i7.jpg" alt="img01"/>
+								</noscript>
+							</div>
+						</li>
+						<li>
+							<div data-alt="img08" data-description="<h3>8</h3>" data-max-width="1800" data-max-height="2399">
+								<div data-src="images/xxlarge/i8.jpg" data-min-width="1000"></div>								
+								<div data-src="images/medium/i8.jpg" data-min-width="200"></div>								
+								<noscript>
+									<img src="images/medium/i8.jpg" alt="img01"/>
+								</noscript>
+							</div>
+						</li>
+						<li>
+							<div data-alt="img09" data-description="<h3>9</h3>" data-max-width="1800" data-max-height="1350">
+								<div data-src="images/xxlarge/i9.jpg" data-min-width="1000"></div>								
+								<div data-src="images/medium/i9.jpg" data-min-width="200"></div>								
+								<noscript>
+									<img src="images/medium/i9.jpg" alt="img01"/>
+								</noscript>
+							</div>
+						</li>
+						<li>
+							<div data-alt="img10" data-description="<h3>10</h3>" data-max-width="1800" data-max-height="1350">
+								<div data-src="images/xxlarge/i10.jpg" data-min-width="1000"></div>								
+								<div data-src="images/medium/i10.jpg" data-min-width="200"></div>								
+								<noscript>
+									<img src="images/medium/i10.jpg" alt="img01"/>
+								</noscript>
+							</div>
+						</li>
+						
+						
+					</ul>
+
+					<div class="gamma-overlay"></div>
+
+					<!--<div id="loadmore" class="loadmore">Example for loading more items...</div>-->
+
+				</div>
+
+			</div><!--/main-->
+		</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+		<script src="assets/js/jquery.masonry.min.js"></script>
+		<script src="assets/js/jquery.history.js"></script>
+		<script src="assets/js/js-url.min.js"></script>
+		<script src="assets/js/jquerypp.custom.js"></script>
+		<script src="assets/js/gamma.js"></script>
+		<script type="text/javascript">
+			
+			$(function() {
+
+				var GammaSettings = {
+						// order is important!
+						viewport : [ {
+							width : 1200,
+							columns : 5
+						}, {
+							width : 900,
+							columns : 4
+						}, {
+							width : 500,
+							columns : 3
+						}, { 
+							width : 320,
+							columns : 2
+						}, { 
+							width : 0,
+							columns : 2
+						} ]
+				};
+
+				Gamma.init( GammaSettings, fncallback );
+
+
+				// Example how to add more items (just a dummy):
+
+				var page = 0,
+					items = [''];
+
+				function fncallback() {
+
+					$( '#loadmore' ).show().on( 'click', function() {
+
+						++page;
+						var newitems = items[page-1]
+						if( page <= 1 ) {
+							
+							Gamma.add( $( newitems ) );
+
+						}
+						if( page === 1 ) {
+
+							$( this ).remove();
+
+						}
+
+					} );
+
+				}
+
+			});
+
+		</script>	
+  <!-- -->
+  
+				</div><!-- col-lg-8 -->
+				
+			</div><!-- row -->
+		</div><!-- container -->
+	</div>	<!-- f -->
+
+	<!-- ========== CONTACT SECTION ========== -->
+	<section id="contact" name="contact"></section>
+	<div id="f">
+		<div class="container">
+			<div class="row">
+				<h3>Контактная информация</h3>
+				<p class="centered"><i class="icon icon-circle"></i><i class="icon icon-circle"></i><i class="icon icon-circle"></i></p>
+				
+				<div class="col-lg-6 col-lg-offset-3">
+					<p><br/>Телефон <br/>+38 8984-4343</p>
+					<p><br/>Почта <br/>teran866@outlook.com</p>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+
+	
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+	<script src="assets/js/classie.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/smoothscroll.js"></script>
+	<script src="assets/js/main.js"></script>
+	<script src="assets/js/galery.js"></script>
+</body>
+</html>
